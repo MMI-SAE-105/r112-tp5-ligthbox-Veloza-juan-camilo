42d0b1ae59b6490fe9f8ba1219d8c4cf57a59f09
Veloza Juan <juan.veloza_arcila@edu.univ-fcomte.fr>
2024-12-03 16:52:13 +0100

    code JavaScript ouvrir,fermer avec des animations

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,26 @@
 // *** LIGHTBOX ***
+const lightbox = document.querySelector("#lightbox");
+const listThumbnail = document.querySelectorAll ("[data-full-img]");
+const lightboxImg = document.querySelector("#lightbox >img");
+
+listThumbnail.forEach ((thumbnail) => {
+    thumbnail.addEventListener('click',(evt) => {
+        lightboxImg.src = thumbnail.dataset.fullImg
+        lightbox.showModal()
+    }); 
+});
+document.body.addEventListener('click', (evt) => {
+    // console.log("cibles de l'evenement", evt.target), evt.target.machine("(data-full-img)");
+    if (evt.target.matches("[data-full-img]")) {
+        lightboxImg.src = evt.target.dataset.fullImg;
+        lightbox.showModal();
+    }
+});
+
+lightbox.addEventListener('click', (evt) => {
+    lightbox.classList.add("sortie");
+        lightbox.addEventListener("animationend", (evt) => {
+            lightbox.classList.remove("sortie");
+            lightbox.close();
+    }, {once: true});
+});
